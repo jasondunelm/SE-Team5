7d38892d0e0f6fc93f889aa688fa1baed4ca124c
joey <joey41517@gmail.com>
2019-05-10 02:29:41 +0100

    Update addEvent.php

--- fullcalendar2/addEvent.php
+++ fullcalendar2/addEvent.php
@@ -4,15 +4,16 @@
 require_once('bdd.php');
 //echo $_POST['title'];
 var_dump($_POST);
-if (isset($_POST['startTime']) && isset($_POST['endTime']) && isset($_POST['facility'])){
+if (isset($_POST['startTime']) && isset($_POST['endTime']) && isset($_POST['facility']) && isset($_POST['totalmoney'])){
     
 	//$title = $_POST['title'];
     $userID = $_POST['user'];
 	$start = $_POST['startDate'].' '.$_POST['startTime'].":00";
 	$end = $_POST['startDate'].' '.$_POST['endTime'].":00";
 	$facility = $_POST['facility'];
+    $bookcost = $_POST['totalmoney'];
 
-	$sql = "INSERT INTO booking(UserID, FacilityID, StartTime, EndTime) values ($userID, $facility, '$start', '$end')";
+	$sql = "INSERT INTO booking(UserID, FacilityID, StartTime, EndTime, BookCost) values ($userID, $facility, '$start', '$end', $bookcost)";
 	//$req = $bdd->prepare($sql);
 	//$req->execute();
 	
