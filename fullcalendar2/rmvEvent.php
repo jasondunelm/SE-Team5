cee98492dfb1b3b204ab38afe3b1858be0796516
joey <joey41517@gmail.com>
2019-05-17 16:08:00 +0100

    Update rmvEvent.php
    for Ren

--- fullcalendar2/rmvEvent.php
+++ fullcalendar2/rmvEvent.php
@@ -21,6 +21,25 @@ if (isset($_POST['evtID'])){
 	}
 
 }
+else if (isset($_POST['deleteBlock'])){
+	$evtID = $_POST['deleteBlock'];
+    
+	$sql = "DELETE FROM `booking` WHERE ID=$evtID";
+
+	
+	$query = $bdd->prepare( $sql );
+	if ($query == false) {
+        print_r($bdd->errorInfo());
+        die ('Erreur prepare');
+	}
+	$sth = $query->execute();
+	if ($sth == false) {
+        print_r($query->errorInfo());
+        die ('Erreur execute');
+	}
+    //header('Location: '.$_SERVER['HTTP_REFERER']);
+    echo "<script> location.href=\"index.php\";</script>";
+}
 //header('Location: '.$_SERVER['HTTP_REFERER']);
 
 	
